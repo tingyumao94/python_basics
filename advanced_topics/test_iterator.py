b4c6013778c799df7eecf104ef258d87f0ad73b2
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-28 18:54:29 +0800

    Delete advanced_topics directory

--- advanced_topics/test_iterator.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-class Iter:
-    def __init__(self, max_limit):
-        self.cnt = 0
-        self.max_limit = max_limit
-
-    def __next__(self):
--- advanced_topics/test_kwargs.py
+++
@@ -1,7 +0,0 @@
-def tmp(a = 1, b= 2):
-
-    print a+b
-
-if __name__ == '__main__':
-
-    tmp(**{'a':2, 'b':2})
